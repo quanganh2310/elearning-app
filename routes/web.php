ee09d1c06d3df8fa2ba856fbd57801cdfb87aef8
quanganh2310 <ailavip1st@gmail.com>
2020-06-13 23:23:11 +0700

    Set route for Course and Subject Controller

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,31 @@ use Illuminate\Support\Facades\Input;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::any ( '/search', function () {
+    $q = Input::get ( 'q' );
+    $course = Course::where ( 'title', 'LIKE', '%' . $q . '%' )->get();
+    if (count ( $course ) > 0)
+        return view ( 'search' )->withDetails ( $course )->withQuery ( $q );
+    else
+        return view ( 'search' )->withMessage ( 'No Details found. Try to search again !' );
+} );
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/all-subjects', 'SubjectsController@showAll')->name('all-subjects');
+Route::get('/all-courses', 'CoursesController@showAll')->name('all-courses');
 
 Route::resources([
     'users' => 'UsersController',
+    'courses' => 'CoursesController',
+    'subjects' => 'SubjectsController'
 ]);
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('users', 'UsersController@index')->name('users.index');
+    Route::post('users/{user}/make-admin', 'UsersController@makeAdmin')->name('users.make-admin');
+    Route::post('users/{user}/make-teacher', 'UsersController@makeTeacher')->name('users.make-teacher');
+});
