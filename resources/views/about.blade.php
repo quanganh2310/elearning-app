f814651924eea6368c536fb04a88e5287421164e
pvt2211 <48007515+pvt2211@users.noreply.github.com>
2020-06-16 17:57:21 +0700

    Update Views/about.balde

---
+++ resources/views/about.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="slider_area ">
+    <div class="bradcam_area breadcam_bg overlay2">
+        <h3>About Us</h3>
+    </div>
+</div>
+
+<div class="our_team_member">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="single_team">
+                        <div class="thumb">
+                            <img src="img/team/nguyen.jpg" alt="" width="255">
+                            <div class="social_link">
+                                <a href="https://www.facebook.com/nguyen.umihara"><i class="ti-facebook"></i></a>
+                                <a href="https://twitter.com/umih4ra"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                            </div>
+                        </div>
+                        <div class="master_name text-center">
+                            <h3>Dương Hải Nguyên</h3>
+                            <p>Coding with Heart</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="single_team">
+                        <div class="thumb">
+                            <img src="img/team/toan.jpg" alt="" width="255">
+                            <div class="social_link">
+                                <a href="https://www.facebook.com/quoctoanpk2511"><i class="ti-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                            </div>
+                        </div>
+                        <div class="master_name text-center">
+                            <h3>Phan Quốc Toản</h3>
+                            <p>Ex-girlfriend is the best</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="single_team">
+                        <div class="thumb">
+                            <img src="img/team/ducanh.jpg" alt="" width="255">
+                            <div class="social_link">
+                                <a href="https://www.facebook.com/zenny.nguyen.3551"><i class="ti-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                            </div>
+                        </div>
+                        <div class="master_name text-center">
+                            <h3>Nguyễn Đức Anh</h3>
+                            <p>Thanh Bear's lover</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="single_team">
+                        <div class="thumb">
+                            <img src="img/team/quanh.jpg" alt="" width="255">
+                            <div class="social_link">
+                                <a href="https://www.facebook.com/quanganhthevoice"><i class="ti-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                            </div>
+                        </div>
+                        <div class="master_name text-center">
+                            <h3>Nguyễn Quang Anh</h3>
+                            <p>Shiba-inu Ava chó</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
